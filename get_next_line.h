0d66a5de31a24fcadc36c3b58b8856c6cf9d2a4e
Gilbert Gerhold <ggerhold@e2r13p5.21-school.ru>
2019-02-07 18:08:41 +0300

    GNL

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ggerhold <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/06 14:28:22 by ggerhold          #+#    #+#             */
+/*   Updated: 2019/02/07 17:57:29 by ggerhold         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 42
+
+# include "libft/libft.h"
+# include <fcntl.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+int		get_next_line(const int fd, char **line);
+
+#endif
